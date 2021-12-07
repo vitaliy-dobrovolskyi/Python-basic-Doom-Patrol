f54445ab4e23ac32aa7adb54a6afed35d30ebc55
vitaliy-dobrovolskyi <95142125+vitaliy-dobrovolskyi@users.noreply.github.com>
2021-12-07 20:43:40 +0200

    Add files via upload

--- homeworks/oop/HW#4 OOP part1.py
+++ homeworks/oop/HW#4 OOP part1.py
@@ -84,7 +84,7 @@ class Bear:
         print(f'I will song {self.make_sound}')
 
 
-Brown = Bear(make_sound="Queen")
+brown = Bear(make_sound="Queen")
 
 
 class Wolf:
@@ -95,7 +95,7 @@ class Wolf:
         print(f'I will play {self.make_sound}')
 
 
-Grey_wolf = Wolf(make_sound="Metallika")
+grey_wolf = Wolf(make_sound="Metallika")
 
 animals = (Grey_wolf, Brown)
 
@@ -106,6 +106,25 @@ for animal in animals:
 # # Magic methods:
 # # 8. Create class City with name, population instance attributes, return a new instance only when population > 1500,
 # # otherwise return message: "Your city is too small".
+class City:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
 
+    def check(self):
+        if self.population > 1500:
+            return self.population
+        else:
+            print(f"{self.name} is so small city")
+
+
+Lviv = City('Lviv', 25000)
+Holovetske = City('Holovetske', 1000)
+Rivne = City('Rivne', 20000)
+
+cities = (Lviv, Holovetske, Rivne)
+
+for i in cities:
+    print(i.check())
 
 
